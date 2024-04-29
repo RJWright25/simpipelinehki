237626503800db163893afe13aaca4c665962480
Ruby Wright <43528535+RJWright25@users.noreply.github.com>
2024-04-29 12:40:38 +0300

    Adjusting star alpha values for animations

--- simpipelinehki/simulations.py
+++ simpipelinehki/simulations.py
@@ -536,7 +536,7 @@ class gadget_simulation:
         return fig,axes
     
     # Method to render all simulation snapshots
-    def gen_sim_animation(self,numproc=1,fps=10,type='baryons',frame=None,galaxies=None,useminpot=False,staralpha=1,subsample=1,verbose=False):
+    def gen_sim_animation(self,numproc=1,fps=10,type='baryons',frame=None,galaxies=pd.DataFrame(),useminpot=False,staralpha=1,subsample=1,verbose=False):
         """
         Render all simulation snapshots.
 
