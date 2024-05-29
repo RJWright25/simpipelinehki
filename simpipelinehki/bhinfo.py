53bb22c05358f8542da681ec8427ec257718e1a5
Ruby Wright <43528535+RJWright25@users.noreply.github.com>
2024-05-29 14:10:11 +0300

    Debugging particle load

--- simpipelinehki/bhinfo.py
+++ simpipelinehki/bhinfo.py
@@ -185,7 +185,7 @@ def read_bhdata(simulation,path=None,bhids=None,subsample=1):
         bhdata_ibh['bh_M']=bhdata_ibh['bh_M']*1e10/simulation.hubble
         bhdata_ibh['bh_Mdot']=bhdata_ibh['bh_Mdot']#don't think this needs to be converted
         for key in [f'Coordinates_{x}' for x in 'xyz']:
-            bhdata_ibh[key]=bhdata_ibh[key]*bhdata_ibh['Time'].values/simulation.hubble
+            bhdata_ibh[key]=bhdata_ibh[key]*bhdata_ibh['ScaleFactor'].values/simulation.hubble
     
         #now add closest snap index from the main simulation to the BH data
         bhdata_ibh['isnap']=np.zeros(bhdata_ibh.shape[0])
